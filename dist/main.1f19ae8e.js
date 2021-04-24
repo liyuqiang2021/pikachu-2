fbe76b25a012aac8f4d48132a5e6c0062b0095c5
john <liyuqiang2021@outlook.com>
2021-04-24 18:58:33 +0800

    解决导入模块错误

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -117,23 +117,9 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"css.js":[function(require,module,exports) {
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = void 0;
+})({"main.js":[function(require,module,exports) {
+// import string from './css.js'
 var string = "\n/*\u9996\u5148\u51C6\u5907\u76AE\u5361\u4E18\u7684\u76AE\u80A4*/\n\n.skin *{box-sizing: border-box;margin: 0;padding: 0;}\n.skin *::before,.skin *::after{box-sizing: border-box;}\n.skin{\n    background: #ffe600;\n    min-height: 100vh;\n    position: relative;\n}\n\n/*\u51C6\u5907\u76AE\u5361\u4E18\u7684\u9F3B\u5B50*/\n\n.nose{\n    border: 10px solid black;\n    border-color: black transparent transparent;\n    border-bottom: none;\n    width: 0px;\n    height: 0px;\n    position: relative;\n    left: 50%;\n    top: 145px;\n    margin-left: -10px;\n    z-index: 10;\n}\n\n/*\u6DFB\u52A0\u9F3B\u5B50\u52A8\u6548*/\n\n@keyframes wave{\n\n 0%{\n    transform: rotate(0deg);\n }\n 33%{\n    transform: rotate(5deg);\n }\n 66%{\n    transform: rotate(-5deg);\n }\n 100%{\n    transform: rotate(0deg);\n }\n}\n.nose:hover{\n    transform-origin: center bottom;\n    animation: wave 300ms infinite linear;\n}\n\n/*\u5236\u4F5C\u773C\u775B*/\n\n.yuan{\n    position:absolute;\n    width: 20PX;\n    height: 6px;\n    top: -16px;\n    left: -10px;\n    border-radius: 14px 14px 0 0;\n    background: black;\n}\n.eye{\n    border:2px solid #000;\n    width: 64px;\n    height: 64px;\n    position: absolute;\n    left:50% ;\n    top: 100px;\n    margin-left: -32px;\n    background: #2e2e2e;\n    border-radius: 50%;\n    z-index:5;\n}\n.eye::before{\n    content: '';\n    display: block;\n    border:0px solid black;\n    width: 25px;\n    height: 25px;\n    background: #fff;\n    border-radius: 50%;\n    position: relative;\n    left: 8px;\n    top: 3px;\n}\n.eye.left{\n    transform: translateX(-100px);\n}\n.eye.right{\n    transform: translateX(100px);\n}\n\n/*\u5236\u4F5C\u4E0A\u5634\u5507*/\n\n.mouth{\n    width: 180px;\n    height: 180px;\n    position: absolute;\n    left: 50%;\n    top: 170px;\n    margin-left: -90px;\n}\n.mouth .up{\n position: relative;\n top: -10px;\n z-index: 1;\n}\n.mouth .up .lip{\n    border:4px solid black;\n    height: 25px;\n    width: 102px;\n    background: #ffe600;\n    border-top-color: transparent;\n    border-right-color: transparent;\n    position: absolute;\n    left: 50%;\n    margin-left: -50px;\n    border-top: none;\n    border-right: none;\n    border-left: none;\n}\n.mouth .up .lip.left{\n    border-radius: 0 0 0 50px;\n    transform: rotate(-20deg)translateX(-54px);\n}\n.mouth .up .lip.right{\n    border-radius: 0 0 50px 0;\n    transform: rotate(20deg)translateX(54px);\n}\n.mouth .up .lip::before{\n    content: '';\n    display: block;\n    width: 10px;\n    height: 30px;\n    position: absolute;\n    bottom: 0;\n    background: #ffe600;\n}\n.mouth .up .lip.left::before{\n    right: -6px;\n}\n.mouth .up .lip.right::before{\n    left: -6px;\n}\n\n/*\u5236\u4F5C\u4E0B\u5634\u5507*/\n\n.mouth .down{\n    height: 160px;\n    position: absolute;\n    top: 15px;\n    width: 100%;\n    overflow: hidden;\n}\n.mouth .down .yuan1{\n    border: 3px solid black;\n    width: 160px;\n    height: 800px;\n    position: absolute;\n    bottom: 0;\n    left: 50%;\n    margin-left: -80px;\n    border-radius:100px/450px;\n    background: #9b000a;\n    overflow: hidden;\n}\n\n/*\u5236\u4F5C\u820C\u5934*/\n\n.mouth .down .yuan1 .yuan2{\n\n    width: 200px;\n    height: 275px;\n    background: #ff485f;\n    position: absolute;\n    bottom: -150px;\n    left: 50%;\n    margin-left: -100px;\n    border-radius: 100px;\n}\n\n/*\u6DFB\u52A0\u816E\u7EA2*/\n\n.face {\n    position: absolute;\n    left: 50%;\n    border: 3px solid black;\n    width: 88px;\n    height: 88px;\n    top: 200px;\n    margin-left: -44px;\n    z-index: 3;\n}\n.face.left{\n    transform: translateX(-180px);\n    background: #ff0000;\n    border-radius: 50px;\n}\n.face.right{\n    transform: translateX(180px);\n    background: #ff0000;\n    border-radius: 50px;\n}\n\n/*\u76AE\u5361\u4E18\u5236\u4F5C\u5B8C\u6210*/\n";
-var _default = string;
-exports.default = _default;
-},{}],"main.js":[function(require,module,exports) {
-"use strict";
-
-var _css = _interopRequireDefault(require("./css.js"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
 var player = {
   id: undefined,
   time: 100,
@@ -150,8 +136,8 @@ var player = {
   },
   n: 1,
   init: function init() {
-    player.ui.demo.innerText = _css.default.substr(0, player.n);
-    player.ui.demo2.innerHTML = _css.default.substr(0, player.n);
+    player.ui.demo.innerText = string.substr(0, player.n);
+    player.ui.demo2.innerHTML = string.substr(0, player.n);
     player.bindEvents();
     player.play();
   },
@@ -167,13 +153,13 @@ var player = {
   run: function run() {
     player.n += 1;
 
-    if (player.n > _css.default.length) {
+    if (player.n > string.length) {
       window.clearInterval(player.id);
       return;
     }
 
-    player.ui.demo.innerText = _css.default.substr(0, player.n);
-    player.ui.demo2.innerHTML = _css.default.substr(0, player.n);
+    player.ui.demo.innerText = string.substr(0, player.n);
+    player.ui.demo2.innerHTML = string.substr(0, player.n);
     player.ui.demo.scrollTop = player.ui.demo.scrollHeight;
   },
   play: function play() {
@@ -200,7 +186,7 @@ var player = {
   }
 };
 player.init();
-},{"./css.js":"css.js"}],"C:/Users/john/AppData/Local/Yarn/Data/global/node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{}],"C:/Users/john/AppData/Local/Yarn/Data/global/node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -228,7 +214,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54592" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55148" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/main.js
+++ src/main.js
@@ -1,5 +1,208 @@
-import string from './css.js'
+// import string from './css.js'
+const string = `
+/*首先准备皮卡丘的皮肤*/
 
+.skin *{box-sizing: border-box;margin: 0;padding: 0;}
+.skin *::before,.skin *::after{box-sizing: border-box;}
+.skin{
+    background: #ffe600;
+    min-height: 100vh;
+    position: relative;
+}
+
+/*准备皮卡丘的鼻子*/
+
+.nose{
+    border: 10px solid black;
+    border-color: black transparent transparent;
+    border-bottom: none;
+    width: 0px;
+    height: 0px;
+    position: relative;
+    left: 50%;
+    top: 145px;
+    margin-left: -10px;
+    z-index: 10;
+}
+
+/*添加鼻子动效*/
+
+@keyframes wave{
+
+ 0%{
+    transform: rotate(0deg);
+ }
+ 33%{
+    transform: rotate(5deg);
+ }
+ 66%{
+    transform: rotate(-5deg);
+ }
+ 100%{
+    transform: rotate(0deg);
+ }
+}
+.nose:hover{
+    transform-origin: center bottom;
+    animation: wave 300ms infinite linear;
+}
+
+/*制作眼睛*/
+
+.yuan{
+    position:absolute;
+    width: 20PX;
+    height: 6px;
+    top: -16px;
+    left: -10px;
+    border-radius: 14px 14px 0 0;
+    background: black;
+}
+.eye{
+    border:2px solid #000;
+    width: 64px;
+    height: 64px;
+    position: absolute;
+    left:50% ;
+    top: 100px;
+    margin-left: -32px;
+    background: #2e2e2e;
+    border-radius: 50%;
+    z-index:5;
+}
+.eye::before{
+    content: '';
+    display: block;
+    border:0px solid black;
+    width: 25px;
+    height: 25px;
+    background: #fff;
+    border-radius: 50%;
+    position: relative;
+    left: 8px;
+    top: 3px;
+}
+.eye.left{
+    transform: translateX(-100px);
+}
+.eye.right{
+    transform: translateX(100px);
+}
+
+/*制作上嘴唇*/
+
+.mouth{
+    width: 180px;
+    height: 180px;
+    position: absolute;
+    left: 50%;
+    top: 170px;
+    margin-left: -90px;
+}
+.mouth .up{
+ position: relative;
+ top: -10px;
+ z-index: 1;
+}
+.mouth .up .lip{
+    border:4px solid black;
+    height: 25px;
+    width: 102px;
+    background: #ffe600;
+    border-top-color: transparent;
+    border-right-color: transparent;
+    position: absolute;
+    left: 50%;
+    margin-left: -50px;
+    border-top: none;
+    border-right: none;
+    border-left: none;
+}
+.mouth .up .lip.left{
+    border-radius: 0 0 0 50px;
+    transform: rotate(-20deg)translateX(-54px);
+}
+.mouth .up .lip.right{
+    border-radius: 0 0 50px 0;
+    transform: rotate(20deg)translateX(54px);
+}
+.mouth .up .lip::before{
+    content: '';
+    display: block;
+    width: 10px;
+    height: 30px;
+    position: absolute;
+    bottom: 0;
+    background: #ffe600;
+}
+.mouth .up .lip.left::before{
+    right: -6px;
+}
+.mouth .up .lip.right::before{
+    left: -6px;
+}
+
+/*制作下嘴唇*/
+
+.mouth .down{
+    height: 160px;
+    position: absolute;
+    top: 15px;
+    width: 100%;
+    overflow: hidden;
+}
+.mouth .down .yuan1{
+    border: 3px solid black;
+    width: 160px;
+    height: 800px;
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    margin-left: -80px;
+    border-radius:100px/450px;
+    background: #9b000a;
+    overflow: hidden;
+}
+
+/*制作舌头*/
+
+.mouth .down .yuan1 .yuan2{
+
+    width: 200px;
+    height: 275px;
+    background: #ff485f;
+    position: absolute;
+    bottom: -150px;
+    left: 50%;
+    margin-left: -100px;
+    border-radius: 100px;
+}
+
+/*添加腮红*/
+
+.face {
+    position: absolute;
+    left: 50%;
+    border: 3px solid black;
+    width: 88px;
+    height: 88px;
+    top: 200px;
+    margin-left: -44px;
+    z-index: 3;
+}
+.face.left{
+    transform: translateX(-180px);
+    background: #ff0000;
+    border-radius: 50px;
+}
+.face.right{
+    transform: translateX(180px);
+    background: #ff0000;
+    border-radius: 50px;
+}
+
+/*皮卡丘制作完成*/
+`
 const player = {
     id: undefined,
     time: 100,
@@ -62,5 +265,4 @@ const player = {
         player.play()
     }
 }
-
 player.init()
